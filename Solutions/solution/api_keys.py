737506a9a8d493deee64f47c6035c2191276cb03
joonc3779 <62186448+joonc3779@users.noreply.github.com>
2020-08-12 17:51:12 -0500

    first

---
+++ Solutions/solution/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "http://api.openweathermap.org/data/2.5/weather?q={Chicago}&appid={AIzaSyDhERBMW9KBFG9O1l_Si1Sh-6oqAsRagws}"
+
+# Google API Key
+g_key = "AIzaSyDhERBMW9KBFG9O1l_Si1Sh-6oqAsRagws"
